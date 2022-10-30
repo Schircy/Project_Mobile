a7a9a2e3e0e76e8e2660fc91309b2851d2cf5ea7
YAI\golle <gollerpoker1209@gmail.com>
2022-10-31 06:27:58 +0700

    Phase1

---
+++ app/src/main/java/com/qnovel_project/MainActivity.java
@@ -0,0 +1,15 @@
+package com.qnovel_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle SavedInstanceState){
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
